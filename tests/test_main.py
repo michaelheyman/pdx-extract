a274ffef053d10cde8a86295d2fc130140d3160c
michaelheyman <mitch.heyman@gmail.com>
2019-10-21 18:00:29 -0700

    Add crawling methods to main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,243 @@
 import asyncio
+import unittest.mock as mock
 
 import asynctest
 import pytest
 
 from app import main
 from tests import utils
+
+
+example_schedule = [
+    {
+        "id": 270591,
+        "term": "201904",
+        "termDesc": "Fall 2019 Quarter",
+        "courseReferenceNumber": "10883",
+        "courseNumber": "161",
+        "subject": "CS",
+        "subjectDescription": "Computer Science",
+        "courseTitle": "INTRO PROGRAM &amp; PROB SOLVING",
+        "creditHours": 4,
+        "subjectCourse": "CS161",
+        "faculty": [
+            {
+                "displayName": "Ely, David D",
+                "emailAddress": "ely@pdx.edu",
+                "term": "201904",
+            }
+        ],
+        "meetingsFaculty": [
+            {
+                "meetingTime": {
+                    "beginTime": "1000",
+                    "endTime": "1150",
+                    "friday": False,
+                    "monday": False,
+                    "saturday": False,
+                    "sunday": False,
+                    "thursday": True,
+                    "tuesday": True,
+                    "wednesday": False,
+                }
+            }
+        ],
+    }
+]
+
+
+@mock.patch("requests.post")
+def test_authenticate_current_session_returns_ok(mock_requests):
+    import requests
+
+    term = {"code": "201904", "description": "Fall 2019 Quarter"}
+    unique_session_id = "abcdef1234567890"
+    cookies = {"JSESSIONID": "CF6813D3F9BFD1ABEEEF47E2FB094926"}
+    response = requests.Response()
+    response.status_code = 200
+    mock_requests.return_value = response
+
+    res = main.authenticate_current_session(term, unique_session_id, cookies)
+
+    assert res.ok is True
+
+
+@mock.patch("requests.post")
+def test_authenticate_current_session_returns_not_ok_if_response_fails(mock_requests):
+    import requests
+
+    term = {"code": "201904", "description": "Fall 2019 Quarter"}
+    unique_session_id = "abcdef1234567890"
+    cookies = {"JSESSIONID": "CF6813D3F9BFD1ABEEEF47E2FB094926"}
+    response = requests.Response()
+    response.status_code = 400
+    mock_requests.return_value = response
+
+    res = main.authenticate_current_session(term, unique_session_id, cookies)
+
+    assert res.ok is False
+
+
+@mock.patch("requests.get")
+def test_get_schedule_json_returns_ok_when_good_response(mock_requests):
+    import json
+    import requests
+
+    subject = {"code": "CS", "description": "Computer Science"}
+    term = {"code": "201904", "description": "Fall 2019 Quarter"}
+    unique_session_id = "abcdef1234567890"
+    cookies = {"JSESSIONID": "CF6813D3F9BFD1ABEEEF47E2FB094926"}
+    response = requests.Response()
+    response._content = bytearray(json.dumps(example_schedule), "utf-8")
+    response.status_code = 200
+    mock_requests.return_value = response
+
+    schedule_json = main.get_schedule_json(subject, term, unique_session_id, cookies)
+
+    assert schedule_json == example_schedule
+
+
+@mock.patch("requests.get")
+def test_get_schedule_json_returns_not_ok_when_bad_response(mock_requests):
+    import requests
+
+    subject = {"code": "CS", "description": "Computer Science"}
+    term = {"code": "201904", "description": "Fall 2019 Quarter"}
+    unique_session_id = "abcdef1234567890"
+    cookies = {"JSESSIONID": "CF6813D3F9BFD1ABEEEF47E2FB094926"}
+    response = requests.Response()
+    response.status_code = 400
+    mock_requests.return_value = response
+
+    schedule_json = main.get_schedule_json(subject, term, unique_session_id, cookies)
+
+    assert schedule_json is None
+
+
+@mock.patch("requests.get")
+def test_get_subjects_returns_json_response_when_response_ok(mock_requests):
+    import json
+    import requests
+
+    cookies = {"cookie": "jar"}
+    unique_session_id = "abcdef1234567890"
+    term_date = "201904"
+    subjects_response = [{"code": "ACTG", "description": "Accounting"}]
+    response = requests.Response()
+    response._content = bytearray(json.dumps(subjects_response), "utf-8")
+    response.status_code = 200
+    mock_requests.return_value = response
+
+    subjects = main.get_subjects(cookies, unique_session_id, term_date)
+
+    assert subjects == subjects_response
+
+
+@mock.patch("requests.get")
+def test_get_subjects_returns_none_when_response_ok(mock_requests):
+    import json
+    import requests
+
+    cookies = {"cookie": "jar"}
+    unique_session_id = "abcdef1234567890"
+    term_date = "201904"
+    subjects_response = [{"code": "ACTG", "description": "Accounting"}]
+    response = requests.Response()
+    response._content = bytearray(json.dumps(subjects_response), "utf-8")
+    response.status_code = 400
+    mock_requests.return_value = response
+
+    subjects = main.get_subjects(cookies, unique_session_id, term_date)
+
+    assert subjects is None
+
+
+@pytest.mark.asyncio
+@asynctest.patch("app.main.authenticate_current_session")
+@asynctest.patch("app.main.get_schedule_json")
+@asynctest.patch("app.pyppeteer.get_unique_session_id")
+async def test_get_subjects_json_returns_data(
+    mock_get_unique_session_id, mock_get_schedule_json, mock_authenticate
+):
+    subjects = [
+        {"code": "ACTG", "description": "Accounting"},
+        {"code": "ACTG", "description": "Accounting"},
+    ]
+    term = {"code": "201904", "description": "Fall 2019 Quarter"}
+    cookies = {"cookie": "jar"}
+    unique_session_id = "abcdef1234567890"
+    mock_get_unique_session_id = asynctest.CoroutineMock()
+    mock_get_unique_session_id = utils.set_async_result(
+        mock_get_unique_session_id, unique_session_id
+    )
+    mock_get_schedule_json.return_value = {"data": "foo"}
+
+    subjects_json = await main.get_subjects_json(subjects, term, cookies, None)
+
+    assert "foo" in subjects_json
+    assert len(subjects_json) == 2
+
+
+@pytest.mark.asyncio
+@asynctest.patch("app.main.authenticate_current_session")
+@asynctest.patch("app.main.get_schedule_json")
+@asynctest.patch("app.pyppeteer.get_unique_session_id")
+async def test_get_subjects_json_returns_none_when_no_data(
+    mock_get_unique_session_id, mock_get_schedule_json, mock_authenticate
+):
+    subjects = [
+        {"code": "ACTG", "description": "Accounting"},
+        {"code": "ACTG", "description": "Accounting"},
+    ]
+    term = {"code": "201904", "description": "Fall 2019 Quarter"}
+    cookies = {"cookie": "jar"}
+    unique_session_id = "abcdef1234567890"
+    mock_get_unique_session_id = asynctest.CoroutineMock()
+    mock_get_unique_session_id = utils.set_async_result(
+        mock_get_unique_session_id, unique_session_id
+    )
+    mock_get_schedule_json.return_value = {}
+
+    subjects_json = await main.get_subjects_json(subjects, term, cookies, None)
+
+    assert subjects_json == []
+
+
+@mock.patch("requests.get")
+def test_get_terms_returns_json_response_when_response_ok(mock_requests):
+    import json
+    import requests
+
+    cookies = {"cookie": "jar"}
+    unique_session_id = "abcdef1234567890"
+    terms_response = [{"code": "201904", "description": "Fall 2019 Quarter"}]
+    response = requests.Response()
+    response._content = bytearray(json.dumps(terms_response), "utf-8")
+    response.status_code = 200
+    mock_requests.return_value = response
+
+    terms = main.get_terms(cookies, unique_session_id)
+
+    assert terms == terms_response
+
+
+@mock.patch("requests.get")
+def test_get_terms_returns_none_when_response_not_ok(mock_requests):
+    import json
+    import requests
+
+    cookies = {"cookie": "jar"}
+    unique_session_id = "abcdef1234567890"
+    terms_response = [{"code": "201904", "description": "Fall 2019 Quarter"}]
+    response = requests.Response()
+    response._content = bytearray(json.dumps(terms_response), "utf-8")
+
+    response.status_code = 400
+    mock_requests.return_value = response
+
+    terms = main.get_terms(cookies, unique_session_id)
+
+    assert terms is None
 
 
 @pytest.mark.asyncio
